e5431de99fcb8764583bd93dcce445a0db0167e5
Arjun Patrawala <ajpat1234@gmail.com>
2021-03-29 18:33:20 -0700

    deploy

---
+++ precache-manifest.4c27650c42ce6295a27c46b1b9c18d24.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5827aa0f38d5780a4c0",
+    "url": "/css/app.2215f7e6.css"
+  },
+  {
+    "revision": "95fda215bff25131cc2d",
+    "url": "/css/chunk-vendors.9d7c95c0.css"
+  },
+  {
+    "revision": "938866389b5ed10380c4636c29b494d3",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "8b96648f513b0144313938d694b12bcf",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "a81d45829a556a5637a1ab4b21006727",
+    "url": "/img/google_logo.a81d4582.svg"
+  },
+  {
+    "revision": "01395362d25dcc5fd988cd17fd857021",
+    "url": "/img/google_logo_white.01395362.svg"
+  },
+  {
+    "revision": "6d3ef46e49473ef9b3a073353f488ab7",
+    "url": "/img/happy.6d3ef46e.svg"
+  },
+  {
+    "revision": "018fa4d196e027a20f5985ef59bd3d59",
+    "url": "/img/logo.018fa4d1.svg"
+  },
+  {
+    "revision": "ccd346fcc585b77856323d6447ac5768",
+    "url": "/img/mad.ccd346fc.svg"
+  },
+  {
+    "revision": "274b217d545af9294e02925d8ad82479",
+    "url": "/img/meh.274b217d.svg"
+  },
+  {
+    "revision": "fc3281910dd6cf4b90d2ea268b8b48f9",
+    "url": "/img/sad.fc328191.svg"
+  },
+  {
+    "revision": "5ff5b16c8a71e516261bc44fc7d6478c",
+    "url": "/img/wow.5ff5b16c.svg"
+  },
+  {
+    "revision": "8be11f2e4e15d34159f6871f8a6ccd6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5827aa0f38d5780a4c0",
+    "url": "/js/app.5b097c35.js"
+  },
+  {
+    "revision": "95fda215bff25131cc2d",
+    "url": "/js/chunk-vendors.d4c4f18b.js"
+  },
+  {
+    "revision": "4f82233faf3ca9c927e0c85735fbefd5",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "b9209944838d899b92779f2f14397fad",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
